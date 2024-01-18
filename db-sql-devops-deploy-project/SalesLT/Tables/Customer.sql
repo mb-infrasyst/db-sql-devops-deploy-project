8ed42893581ecb584689aa3094ea05affd336df0
Melvin Batista <mbatista@infrasystpr.com>
2024-01-17 20:36:07 -0400

    Add TestColumn2

--- db-sql-devops-deploy-project/SalesLT/Tables/Customer.sql
+++ db-sql-devops-deploy-project/SalesLT/Tables/Customer.sql
@@ -15,6 +15,7 @@
     [rowguid]      UNIQUEIDENTIFIER  CONSTRAINT [DF_Customer_rowguid] DEFAULT (newid()) NOT NULL,
     [ModifiedDate] DATETIME          CONSTRAINT [DF_Customer_ModifiedDate] DEFAULT (getdate()) NOT NULL,
     [TestColumn] NCHAR(10) NULL, 
+    [TestColumn2] NCHAR(10) NULL, 
     CONSTRAINT [PK_Customer_CustomerID] PRIMARY KEY CLUSTERED ([CustomerID] ASC),
     CONSTRAINT [AK_Customer_rowguid] UNIQUE NONCLUSTERED ([rowguid] ASC)
 );
